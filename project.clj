7135728cfa7d2b06f084c70020574c502eb70e3b
Joshua Hoff <randomenduser@gmail.com>
2014-05-17 12:47:01 -0500

    Remove the command-line interface
    It's been moved to https://github.com/losingkeys/virustotal-cli

--- project.clj
+++ project.clj
@@ -4,12 +4,7 @@ https://www.virustotal.com/en/documentation/public-api/"
   :url "https://losingkeys.github.io/clj-virustotal"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0-alpha3"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [cheshire "5.3.1"]
-                 [clj-http "0.9.1"]
-                 [digest "1.4.4"]
-                 [org.clojure/tools.cli "0.3.1"]]
-  :plugins [[codox "0.8.0"]]
-  ;; thanks: https://github.com/technomancy/leiningen/blob/stable/doc/TUTORIAL.md#uberjar
-  :main clj-virustotal.main
-  :aot [clj-virustotal.main])
+                 [clj-http "0.9.1"]]
+  :plugins [[codox "0.8.0"]])
